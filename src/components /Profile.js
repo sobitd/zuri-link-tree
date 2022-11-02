e8bec00e9612222e0899a3bd0a05f73e2e791a10
sobitd <sobitdaniel@gmail.com>
2022-11-02 04:48:22 +0300

    create profile image and content avatars

--- src/components /Profile.js
+++ src/components /Profile.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 function Profile() {
-  return <div></div>;
+  return <div className="profile-wrapper">
+    <div className="profile-image"></div>
+    <div className="profile content"></div>
+
+  </div>;
 }
 
 export default Profile;
