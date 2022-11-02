32aed9cc319961e16fc22225ec0f75809d18739c
sobitd <sobitdaniel@gmail.com>
2022-11-02 05:32:09 +0300

    add profile section component

--- src/components /Profile.js
+++ src/components /Profile.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 
+
 function Profile() {
   return <div className="profile-wrapper">
-    <div className="profile-image"></div>
-    <div className="profile content"></div>
+    <img id="profile_img" src="https://i.pinimg.com/564x/65/c0/d2/65c0d23070c3f2a850114d8760694616.jpg"/>
+    <h2>Annette Black</h2>
+    <h3 id="twitter">twitter username: @fullysobit</h3>
+    <h3 id="slack">slack username: Daniel Sobit</h3>
 
   </div>;
 }
